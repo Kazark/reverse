078b49ce15ce62340f43fc6041851abc1fd4d94e
Kazark <kazark@zoho.com>
2019-11-30 15:26:56 -0500

    Get the basic layout engine working

--- src/Reverse/Model.hs
+++ src/Reverse/Model.hs
@@ -1,8 +1,8 @@
 module Reverse.Model (Reverse(..), derive, integrate) where
 
-import Reverse.UI (Outputtable(..))
-import System.Console.Terminfo (termText)
+import Reverse.UI (Outputtable(..), Row(..))
 
+-- TODO use Row here?
 data Reverse = Reverse [[String]] ([String], String, [String]) [[String]]
 
 integrate :: ([String], String, [String]) -> [String]
@@ -12,9 +12,6 @@ derive :: [String] -> ([String], String, [String])
 derive [] = ([], "", [])
 derive (x : xs) = ([], x, xs)
 
-flatten :: Reverse -> [String]
-flatten (Reverse befores current afters) =
-  fmap unwords (befores ++ [integrate current] ++ afters)
-
 instance Outputtable Reverse where
-  output = termText . unlines . flatten
+  output (Reverse befores current afters) =
+    fmap Row $ befores ++ [integrate current] ++ afters
--- src/Reverse/UI.hs
+++ src/Reverse/UI.hs
@@ -1,11 +1,59 @@
-module Reverse.UI (Outputtable(..), TermEnv, initTerm, redraw) where
+{-# LANGUAGE DeriveFunctor #-}
+module Reverse.UI (Row(..), Outputtable(..), TermEnv, initTerm, redraw) where
 
 import System.Console.Terminfo
 import System.Exit (die)
 import System.IO (stdin, hSetBuffering, BufferMode(NoBuffering))
+import Text.Printf (FieldFormat(..), FormatAdjustment(..), formatString)
+
+newtype Row a = Row [a] deriving Functor
 
 class Outputtable a where
-  output :: a -> TermOutput
+  output :: a -> [Row String]
+
+rowLength :: Row a -> Int
+rowLength (Row xs) = length xs
+
+indexOr0 :: Int -> Row Int -> Int
+indexOr0 i (Row xs) =
+  if i < length xs
+  then xs !! i
+  else 0
+
+maxColumns :: [Row a] -> Int
+maxColumns = maximum . fmap rowLength
+
+colWidths :: [Row String] -> [Int]
+colWidths x =
+  let ns = fmap (fmap length) x
+      lastCol = maxColumns x-1
+      colSizes = fmap (\i -> fmap (indexOr0 i) ns) [0..lastCol]
+  in fmap maximum colSizes
+
+markWidths :: [Int] -> Row String -> Row (Int, String)
+markWidths is (Row ss) = Row $ zip is ss
+
+padBy :: Int -> FieldFormat
+padBy x =
+  FieldFormat { fmtWidth = Just x
+              , fmtPrecision = Nothing
+              , fmtAdjust = Just LeftAdjust
+              , fmtSign = Nothing
+              , fmtAlternate = False
+              , fmtModifiers = ""
+              , fmtChar = 's'
+              }
+
+padRow :: Row (Int, String) -> Row String
+padRow = fmap (\(i, s) -> formatString s (padBy i) "")
+
+renderRow :: Row String -> String
+renderRow (Row xs) = unwords xs
+
+layout :: [Row String] -> TermOutput
+layout xs =
+  let ws = colWidths xs
+  in termText $ unlines $ fmap (renderRow . padRow . markWidths ws) xs
 
 data TermEnv
   = TermEnv { term :: Terminal
@@ -28,4 +76,4 @@ initTerm = do
 redraw :: Outputtable a => TermEnv -> a -> IO ()
 redraw env x =
   let whatShouldThisBe = 1337
-  in runTermOutput (term env) $ cls env whatShouldThisBe <> output x
+  in runTermOutput (term env) $ cls env whatShouldThisBe <> layout (output x)
